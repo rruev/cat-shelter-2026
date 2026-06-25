cf25a23b1f33415be4cd0a9d96bc60b5753d85ff
rruev <rosenruev@gmail.com>
2026-06-25 17:23:50 +0200

    two new routes added

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import http from 'http';
 import fs from 'fs/promises';
 
 const server = http.createServer(async (req, res) => {
+	
 	if (req.url === '/styles/site.css') {
 		const cssFile = await fs.readFile('./src/styles/site.css', 'utf-8');
 		res.writeHead(200, {'Content-Type': 'text/css'});
 		res.write(cssFile);
-		res.end();
-		return;
+		return res.end();
+	}
+	
+	let htmlContent;
+	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	if (req.url === '/') {
+		htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+		res.write(htmlContent);
+		return res.end();
 	}
 
-	const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
-	res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(homePage);
-  res.end();
+	if (req.url === '/cats/add-breed') {
+		htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
+		res.write(htmlContent);
+		return res.end();
+	}
+
+
 });
 
 server.listen(3000, () => {
