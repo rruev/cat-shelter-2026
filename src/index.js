c6d0f8d3aa73a42fdce66414d61e9d30f5c58b56
rruev <rosenruev@gmail.com>
2026-06-25 16:50:49 +0200

    styles added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import http from 'http';
 import fs from 'fs/promises';
 
 const server = http.createServer(async (req, res) => {
+	if (req.url === '/styles/site.css') {
+		const cssFile = await fs.readFile('./src/styles/site.css', 'utf-8');
+		res.writeHead(200, {'Content-Type': 'text/css'});
+		res.write(cssFile);
+		res.end();
+		return;
+	}
+
 	const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
 	res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(homePage);
